4387aaeee5f21f5081e6aae6c53a79d88eef6700
chenjianwei <938548157@qq.com>
2017-07-25 17:39:48 +0800

    fix android

--- InvertibleScrollView.js
+++ InvertibleScrollView.js
@@ -1,104 +1,83 @@
 'use strict';
 
 import React, {
-    PropTypes,
+  PropTypes,
 } from 'react';
 import {
-    ScrollView,
-    StyleSheet,
-    View,
+  ScrollView,
+  StyleSheet,
+  View,
     Platform,
 } from 'react-native';
 import ScrollableMixin from 'react-native-scrollable-mixin';
 
 import cloneReferencedElement from 'react-clone-referenced-element';
 
-type
-DefaultProps = {
-    renderScrollComponent: (props: Object) => ReactElement;
-}
-;
+type DefaultProps = {
+  renderScrollComponent: (props: Object) => ReactElement;
+};
 
 let InvertibleScrollView = React.createClass({
-    mixins: [ScrollableMixin],
+  mixins: [ScrollableMixin],
 
-    propTypes: {
-        ...ScrollView.propTypes,
-        inverted: PropTypes.bool,
-        renderScrollComponent: PropTypes.func.isRequired,
-    },
+  propTypes: {
+    ...ScrollView.propTypes,
+    inverted: PropTypes.bool,
+    renderScrollComponent: PropTypes.func.isRequired,
+  },
 
-    getDefaultProps(): DefaultProps {
-        return {
-            renderScrollComponent: props => <ScrollView {...props} />,
-        };
-    },
+  getDefaultProps(): DefaultProps {
+    return {
+      renderScrollComponent: props => <ScrollView {...props} />,
+    };
+  },
 
-    getScrollResponder(): ReactComponent {
-        return this._scrollComponent.getScrollResponder();
-    },
+  getScrollResponder(): ReactComponent {
+    return this._scrollComponent.getScrollResponder();
+  },
 
-    setNativeProps(props: Object) {
-        this._scrollComponent.setNativeProps(props);
-    },
+  setNativeProps(props: Object) {
+    this._scrollComponent.setNativeProps(props);
+  },
 
-    render() {
-        var {
-            inverted,
-            renderScrollComponent,
-            ...props,
-        } = this.props;
+  render() {
+    var {
+      inverted,
+      renderScrollComponent,
+      ...props,
+    } = this.props;
 
-        if (inverted) {
-            if (this.props.horizontal) {
-                props.style = [styles.horizontallyInverted, props.style];
-                props.children = this._renderInvertedChildren(props.children, styles.horizontallyInverted);
-            } else {
-                props.style = [styles.verticallyInverted, props.style];
-                props.children = this._renderInvertedChildren(props.children, styles.verticallyInverted);
-            }
-        }
+    if (inverted) {
+      if (this.props.horizontal) {
+        props.style = [styles.horizontallyInverted, props.style];
+        props.children = this._renderInvertedChildren(props.children, styles.horizontallyInverted);
+      } else {
+        props.style = [styles.verticallyInverted, props.style];
+        props.children = this._renderInvertedChildren(props.children, styles.verticallyInverted);
+      }
+    }
 
-        return cloneReferencedElement(renderScrollComponent(props), {
-            ref: component => {
-                this._scrollComponent = component;
-            },
-        });
-    },
+    return cloneReferencedElement(renderScrollComponent(props), {
+      ref: component => { this._scrollComponent = component; },
+    });
+  },
 
-    _renderInvertedChildren(children, inversionStyle) {
-        return React.Children.map(children, child => {
-            return child ? <View style={inversionStyle}>{child}</View> : child;
-        });
-    },
+  _renderInvertedChildren(children, inversionStyle) {
+    return React.Children.map(children, child => {
+      return child ? <View style={inversionStyle}>{child}</View> : child;
+    });
+  },
 });
 
-const verticalTransform = [
-    { scaleY: -1 },
-];
-
-const horizontalTransform = [
-    { scaleX: -1 },
-];
-
-if (Platform.OS === 'android') {
-    verticalTransform.push({
-        perspective: 1280,
-    });
-    horizontalTransform.push({
-        perspective: 1280,
-    })
-}
-
 let styles = StyleSheet.create({
-    verticallyInverted: {
-        flex: 1,
-        transform: verticalTransform,
-    },
-    horizontallyInverted: {
-        flex: 1,
-        transform: horizontalTransform,
-    },
+    verticallyInverted: Platform.select({
+        ios: {transform: [{scaleY: -1}]},
+        android: {scaleY: -1},
+    }),
+    horizontallyInverted: Platform.select({
+        ios: {transform: [{scaleX: -1}]},
+        android: {scaleX: -1},
+    }),
 });
 
 module.exports = InvertibleScrollView;
